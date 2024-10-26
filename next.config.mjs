8bb28a9e748636eb2508d97b0a2c801654b97f71
Reza <pr.rezaabiri@gmail.com>
2024-10-26 14:04:17 +0330

    افزودن دامنه به کانفیگ برای لود تصویر

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.dummyjson.com'], // هاست‌های مجاز برای بارگذاری تصاویر
+        domains: ['cdn.dummyjson.com'],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.dummyjson.com",
+                port: "",
+                pathname: "/products/images/**",
+            },
+        ],
     },
 };
 
