f824f7202eba20bcfdeaeafe69113a6e3aa67210
Reza <pr.rezaabiri@gmail.com>
2024-11-04 12:56:10 +0330

    افزودن متغیر محیطی به داکر

--- src/core/tokenService.ts
+++ src/core/tokenService.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-const secretKey: string = process.env.SECRET_KEY;
+const secretKey: string = process.env.SECRET_KEY || "wdHO$>*bVc>J]";
 export function generateToken(path: string): string {
     const timestamp = Math.floor(Date.now() / 1000);
     const data = `${timestamp}|${path}`;
