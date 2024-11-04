d023a4bc5f6e17320df3fe854e3ed7750e312613
Reza <pr.rezaabiri@gmail.com>
2024-11-04 16:20:01 +0330

    تغییرات دیزاینی در صفحه لاگین صفحه تایید کد

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -14,11 +14,11 @@ const Page = () => {
         setPhone(localStorage.getItem('phone') ?? '')
     }, [])
     return (
-        <div className={'flex justify-center items-center p-6 lg:p-12'}>
+        <div className={'flex justify-center items-center h-screen p-6 lg:p-12'}>
             <div
-                className={'flex flex-col lg:flex-row w-full lg:w-[80%] h-[80%] bg-white justify-start items-start rounded-lg drop-shadow-lg p-6'}>
+                className={'flex flex-col lg:flex-row w-full lg:w-[80%] h-auto bg-white justify-start items-start rounded-lg drop-shadow-lg p-6'}>
                 <div className={'flex order-2 lg:order-1 lg:mr-16 flex-col w-full lg:w-1/2 justify-center'}>
-                    <label>
+                    <label className={'mt-4 lg:mt-0'}>
                         Welcome Back 👋
                     </label>
                     <p className={'my-8'}>
@@ -40,7 +40,7 @@ const Page = () => {
 
                 </div>
                 <div
-                    className={'flex flex-col order-1 lg:order-2 w-full lg:w-1/2 py-20 mb-6 lg:py-60 justify-center items-center rounded-lg bg-blue-200'}>
+                    className={'flex flex-col order-1 lg:order-2 w-full lg:w-1/2 py-20 lg:py-40 justify-center items-center rounded-lg bg-blue-200'}>
                 </div>
 
 
